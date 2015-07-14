3f777d9b1a4a67af63fd09173c2409ac9d1fe68a
David Winterbottom <david.winterbottom@gmail.com>
2015-07-14 09:17:37 +0100

    Install uWSGI as part of package

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 An example Django project which can be built and deployed as a `.deb` file.
+
+To add a Python package, install it using `pip install $package` then update
+`requirements.txt` using `pip-dump`.
--- package.sh
+++ package.sh
@@ -1,11 +1,10 @@
 #/usr/bin/env bash
 
+set -e
+
 echo "Creating package"
 vagrant ssh packaging -- "cd /vagrant/ && sudo dpkg-buildpackage -us -uc"
 vagrant ssh packaging -- "sudo mv /*.deb /vagrant"
 
 echo "Testing package"
 vagrant ssh test -- "cd /vagrant/ && sudo gdebi *.deb"
-
-echo "Cleaning up"
--rm -rf build dist *.deb
