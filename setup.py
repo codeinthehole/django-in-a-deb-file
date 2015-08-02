d2af17bf20264ddb7f2d30bf987cf670134313c0
David Winterbottom <david.winterbottom@gmail.com>
2015-08-02 22:24:35 +0100

    Use a version.py for the Python package version

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
+PACKAGE_NAME = "helloworld"
+
+exec(open('src/%s/version.py' % PACKAGE_NAME).read())
+
 setup(
-    name='helloworld',
-    version='0.1',
+    name=PACKAGE_NAME,
+    version=__version__,
     package_dir={'': 'src'},
-    packages=['helloworld'],
+    packages=[PACKAGE_NAME],
     include_package_data=True,
 )
