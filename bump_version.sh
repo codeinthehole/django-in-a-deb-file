8ea0750e0b940dbc857e1d5f9cdfa23d408699e4
David Winterbottom <david.winterbottom@gmail.com>
2015-08-17 15:27:56 +0100

    Use a template changelog

--- bump_version.sh
+++ bump_version.sh
@@ -4,10 +4,11 @@
 
 # Determine new version number
 PYTHON_VERSION=$1
+DEBIAN_VERSION=${2:-1}
 
 # See https://www.debian.org/doc/debian-policy/ch-controlfields.html#s-f-Version for more on 
 # Debian versioning conventions.
-DEBIAN_VERSION="$PYTHON_VERSION-1"
+DEBIAN_VERSION="$PYTHON_VERSION-$DEBIAN_VERSION"
 
 # Update version for python project
 VERSION_FILE=src/helloworld/version.py
@@ -17,5 +18,11 @@ echo "__version__ = '$PYTHON_VERSION'" > $VERSION_FILE
 # Update debian changelog using `dch` command
 COMMIT=$(git rev-parse HEAD)
 DESCRIPTION="Built from commit $COMMIT"
+
 echo "Updating debian/changelog"
-vagrant ssh packaging -- "cd /vagrant/ && dch --newversion=$DEBIAN_VERSION --controlmaint --urgency=medium --distribution=unstable $DESCRIPTION"
+BUILD_DATE=$(date +"%a, %d %b %Y %H:%M:%S %z")
+cat debian/changelog.template | \
+    sed "s/PACKAGE/helloworld/" | \
+    sed "s/VERSION/$DEBIAN_VERSION/" | \
+    sed "s/DESCRIPTION/$DESCRIPTION/" | \
+    sed "s/BUILD_DATE/$BUILD_DATE/" > debian/changelog
