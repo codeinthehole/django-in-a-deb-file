f986829d369f0a288568644a939c8e259fec025c
David Winterbottom <david.winterbottom@gmail.com>
2015-09-02 16:18:49 +0100

    Add puppet config to install package from S3 repo

--- packer/ami.pp
+++ packer/ami.pp
@@ -1,1 +1,17 @@
-include '::ntp'
+include "::ntp"
+
+file_line { "add s3 apt repository":
+    path => "/etc/apt/sources.list",
+    line => "deb https://octo-debian-packages.s3.amazonaws.com stable main",
+    notify => Exec["apt-get update"],
+}
+
+exec { "apt-get update":
+    command => "/usr/bin/apt-get update",
+}
+
+package { "helloworld":
+    ensure => "installed",
+    install_options => "--force-yes",
+    require => File_Line["add s3 apt repository"],
+}
