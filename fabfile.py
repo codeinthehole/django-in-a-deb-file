a8b5136ef01c0a5b10eabd093e6aac802f11562e
David Winterbottom <david.winterbottom@gmail.com>
2015-09-02 22:39:43 +0100

    Replace clean script with fabric

---
+++ fabfile.py
@@ -0,0 +1,20 @@
+from fabric import api
+import os
+
+PACKAGE = "helloworld"
+
+TEMP_FOLDERS = (
+    "build", "dist", "usr/share/static", "debian/%s" % PACKAGE
+)
+
+TEMP_FILES = (
+    "debian/%s.debhelper.log" % PACKAGE,
+)
+
+
+def clean():
+    for folder in TEMP_FOLDERS:
+        api.local("rm -rf %s" % folder)
+    for file in TEMP_FILES:
+        if os.path.exists(file):
+            api.local("rm %s" % file)
