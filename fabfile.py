bdfd5d85c755281c7a7e94139f1f828809cb92ef
David Winterbottom <david.winterbottom@gmail.com>
2015-09-22 10:13:36 +0100

    Reorganise fabfile

--- fabfile.py
+++ fabfile.py
@@ -28,7 +28,7 @@ DEBIAN_CHANGELOG_TEMPLATE = """PACKAGE (VERSION) unstable; urgency=medium
 
   * DESCRIPTION
 
- -- Build-bot <buildbot@example.com>  BUILD_DATE
+ -- AUTHOR <AUTHOR_EMAIL>  BUILD_DATE
 """
 
 
@@ -47,39 +47,52 @@ def clean():
 
 
 def package():
+    """
+    Build a .deb file
+    """
     clean()
 
     now = datetime.datetime.now(pytz.timezone("Europe/London"))
-    timestamp = now.strftime("%Y%m%d%H%M%S")
+    version = now.strftime("%Y%m%d%H%M%S")
 
-    print "Updating python package version"
-    version_file = "src/%s/version.py" % PACKAGE
-    with open(version_file, "w") as f:
-        f.write("__version__ = '%s'" % timestamp)
-
-    print "Updating debian changelog"
-    replacements = {
-        'PACKAGE': PACKAGE,
-        'VERSION': timestamp,
-        'DESCRIPTION': "Built from commit %s" % _commit_sha(),
-        'BUILD_DATE': now.strftime("%a, %d %b %Y %H:%M:%S %z"),
-    }
-    contents = DEBIAN_CHANGELOG_TEMPLATE
-    for variable, replacement in replacements.items():
-        contents = contents.replace(variable, replacement)
-    with open("debian/changelog", "w") as f:
-        f.write(contents)
+    _update_python_package_version(version)
+    _update_debian_changelog(now, version)
 
     print "Collecting static files"
     api.local("src/manage.py collectstatic --noinput")
 
-    print "Build package"
+    print "Building package in Vagrant box"
+    api.local('vagrant up packaging')
     api.local('vagrant ssh packaging -- "cd /vagrant/ && sudo dpkg-buildpackage -us -uc"')
 
     # dpkg-buildpackage creates the .deb file in the root folder (/) on the
     # file system. We need to move that into the shared folder (/vagrant) so we
     # can access it from the host machine.
     api.local('vagrant ssh packaging -- "sudo mv /*.deb /vagrant/debian/dist/"')
+
+
+def _update_python_package_version(version):
+    print "Updating python package version"
+    version_file = "src/%s/version.py" % PACKAGE
+    with open(version_file, "w") as f:
+        f.write("__version__ = '%s'" % version)
+
+
+def _update_debian_changelog(now, version):
+    print "Updating debian changelog"
+    replacements = {
+        'PACKAGE': PACKAGE,
+        'VERSION': version,
+        'DESCRIPTION': "Built from commit %s" % _commit_sha(),
+        'BUILD_DATE': now.strftime("%a, %d %b %Y %H:%M:%S %z"),
+        'AUTHOR_EMAIL': 'dev@example.com',
+        'AUTHOR': 'Fabric',
+    }
+    contents = DEBIAN_CHANGELOG_TEMPLATE
+    for variable, replacement in replacements.items():
+        contents = contents.replace(variable, replacement)
+    with open("debian/changelog", "w") as f:
+        f.write(contents)
 
 
 def _commit_sha():
